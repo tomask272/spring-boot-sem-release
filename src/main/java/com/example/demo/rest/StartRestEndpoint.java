165ad60fc3ef1ae41d33c345881ddc6d03a5ff63
Tomáš Klouček <tomas.kloucek@embedit.cz>
2021-05-12 16:29:55 +0200

    feat: Exposing build info

---
+++ src/main/java/com/example/demo/rest/StartRestEndpoint.java
@@ -0,0 +1,18 @@
+package com.example.demo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StartRestEndpoint {
+
+    @Autowired
+    private BuildProperties buildProperties;
+
+    @GetMapping("/build")
+    public String getBuild() {
+        return buildProperties.getVersion();
+    }
+}
